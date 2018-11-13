3c7c9c28b2f2821adeba2b7b5d4d25310767272c
Jordan Kuhn <jtkuhn@foxguardsolutions.com>
2018-11-13 15:24:51 -0500

    🔄 Rename underlying folder to FGS.Incipience.Support
    🔄 Original Commit: https://github.com/foxguardsolutions/Incipience/commit/dbdb28d409eef01d9f5be0261d5b26a60942c95e

---
+++ FGS.Incipience.Support/SupportModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+
+using DiffPlex;
+using DiffPlex.DiffBuilder;
+
+using Microsoft.AspNetCore.Mvc.Filters;
+
+using Support.Validation;
+
+namespace FGS.Incipience.Support
+{
+    public class SupportModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ValidateModelStateFilter>().As<IActionFilter>();
+            builder.RegisterType<Clock>().As<IClock>();
+            builder.RegisterType<SideBySideDiffBuilder>().As<ISideBySideDiffBuilder>();
+            builder.RegisterType<Differ>().As<IDiffer>();
+        }
+    }
+}
