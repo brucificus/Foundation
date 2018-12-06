589fa13ec1813fcf36fd9df62a7664b72c3dbb85
Bruce Markham <219281+brucificus@users.noreply.github.com>
2018-12-05 19:33:12 -0500

    🔄 Converts IResult Usages to Use `OneOf`
    Converts all usages of IResult. Adds helpers that give us LINQ-like syntax for using multiple `OneOf` values in succession without large syntactic overhead, inspired by Mark Seemann's "Humane Code" video series.
    
    Purposefully absconds from implementing tests for new helpers (in lieu of the typical anticipated manual testing) since the new helpers replaced old code that was already being thoroughly-yet-indirectly tested via the tests of consumers of said helpers.
    
    
    🔄 Original Commit: https://github.com/foxguardsolutions/Incipience/commit/0de8496994f434adeddc616ad708e563eca76687

---
+++ FGS.Incipience.TestSupport/OneOfAssertionExtensions.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OneOf;
+using OneOf.Types;
+
+namespace FGS.Incipience.TestSupport
+{
+    public static class OneOfAssertionExtensions
+    {
+        public static void AssertSuccess<TError>(
+            this OneOf<Success, TError> result)
+        {
+            result.Switch(
+                _ => Assert.Pass(),
+                _ => Assert.Fail("Expected success result, but was error"));
+        }
+
+        public static void AssertError<TError>(
+            this OneOf<Success, TError> result,
+            TError expectedErrorData)
+        {
+            result.Switch(
+                _ => Assert.Fail("Expected error result, but was success"),
+                data => Assert.That(data, Is.EqualTo(expectedErrorData)));
+        }
+
+        public static void AssertError<TSuccess, TError>(this OneOf<TSuccess, TError> result, TError expectedErrorData)
+        {
+            result.Switch(
+                _ => Assert.Fail("Expected error result, but was success"),
+                data => Assert.That(data, Is.EqualTo(expectedErrorData)));
+        }
+
+        public static void AssertSuccessString<TError>(
+            this OneOf<Success<string>, TError> result,
+            string expectedSuccess)
+        {
+            result.Switch(
+                data => Assert.That(data.Value, Is.EqualTo(expectedSuccess)),
+                _ => Assert.Fail("Expected success result, but was error"));
+        }
+
+        public static void AssertSuccess<TSuccess, TError>(
+            this OneOf<TSuccess, TError> result,
+            TSuccess expectedSuccessData)
+        {
+            result.Switch(
+                data => Assert.That(data, Is.EqualTo(expectedSuccessData)),
+                _ => Assert.Fail("Expected success result, but was error"));
+        }
+    }
+}
